35e5cb0b47766b3c23e1007c778bffa4080803c2
baba_s <shotababa1206@gmail.com>
2020-09-24 19:55:48 +0900

    first commit

---
+++ Editor/iOSPluginPlatformTest.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework;
+using System.Linq;
+using System.Text;
+using UnityEditor;
+
+namespace Kogane.Internal
+{
+	internal sealed class iOSPluginPlatformTest
+	{
+		[Category( nameof( Kogane ) )]
+		[Test]
+		public void iOS_プラグインのプラットフォームが適切か()
+		{
+			var list = AssetDatabase
+					.GetAllAssetPaths()
+					.Where( c => c.Contains( "/Plugins/" ) )
+					.Where( c => c.Contains( "/iOS/" ) )
+					.Select( c => AssetImporter.GetAtPath( c ) )
+					.OfType<PluginImporter>()
+					.Where( c => c != null )
+					.Where( c => c.GetCompatibleWithPlatform( BuildTarget.Android ) )
+					.Select( c => AssetDatabase.GetAssetPath( c ) )
+				;
+
+			if ( !list.Any() ) return;
+
+			var sb = new StringBuilder();
+
+			foreach ( var n in list )
+			{
+				sb.AppendLine( n );
+			}
+
+			Assert.Fail( sb.ToString() );
+		}
+	}
+}
